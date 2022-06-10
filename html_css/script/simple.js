7e0b13316002f0d617750655a826b4f9fa18692b
ZEHR34 <entropia.cfg@gmail.com>
2022-06-10 19:18:19 +0300

    lab 2 complite

--- html_css/script/simple.js
+++ html_css/script/simple.js
@@ -1,14 +1,18 @@
-async function userAction(url, met, Body) {
-    const response = await fetch(`/users`, {
+async function userAction(url, met, Body=null, token= null) {
+
+    const response = await fetch(url, {
         method: met,
         body: JSON.stringify(Body), // string or object
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
     });
+
     const myJson = await response.json(); //extract JSON from the http response
-    // console.log(myJson)
-    document.getElementById("show_text").textContent += myJson
+    // document.getElementById("show_text").textContent += myJson
+    console.log(myJson);
+    return myJson;
 };
 
 
@@ -18,6 +22,9 @@ body={
     password:"mosaad2020"
 };
 
-userAction("users",'POST', body);
-
-// document.getElementById("show_text").textContent += " this has just been added";
+userAction("users",'POST', body).then(
+    (data) => userAction("users/login","POST",body)).then(
+    (data) => userAction("users/me","DELETE",body, data.token)
+);
+// userAction("users/login","POST",{email: body.email, password: body.password}).then(
+//     (data) => userAction("users/me","DELETE",null,data.token));
